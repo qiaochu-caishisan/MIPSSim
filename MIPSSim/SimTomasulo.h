24e30e242e7774280cc6e86f9196bf5c89cb715c
qiaochu-caishisan <logan_chu@outlook.com>
2016-11-29 00:30:49 -0500

    Add files via upload

---
+++ MIPSSim/SimTomasulo.h
@@ -0,0 +1,51 @@
+
+/************************************************************************/
+/*SimTomasulo.h                                                         */
+/*Authorized by: Qiao Chu                                               */
+/************************************************************************/
+
+#ifndef SIMTOMASULO_H_
+#define SIMTOMASULO_H_
+
+#include "InstParser.h"
+
+#define MAX_ROB_ENTRIES 6
+#define MAX_RS_ENTRIES  10
+
+// This structure defines an entry for Reservation Station
+typedef struct _RSEntry
+{
+    bool            UpdateBusyStatus;
+    bool            Busy;
+    Instruction     *pInst;
+    INT             Vj;
+    INT             Vk;
+    UINT            Qj;
+    UINT            Qk;
+    UINT            Dest;
+    INT             Addr;
+    INT             Result;
+}RSEntry;
+
+// This structure defines an entry for ReOrder Buffer 
+typedef struct _ROBEntry
+{
+    bool                UpdateBusyStatus;
+    bool                Busy;
+    Instruction         *pInst;
+    bool                Ready;
+    UINT                Dest;
+    INT                 Value;
+    INT                 Addr;
+    INT                 decideempty;
+}ROBEntry;
+
+// This struture defines an entry for Register Status 
+typedef struct _RegisterStatEntry
+{
+    bool    Busy;
+    UINT    Reorder;
+}RegisterStatEntry;
+
+#endif
+
